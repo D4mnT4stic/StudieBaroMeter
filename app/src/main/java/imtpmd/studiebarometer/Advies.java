f2e93bcfec5abddbf9e4b19261d468e93b91fd6e
joost van Eeden <joost_van_eeden@hotmail.com>
2016-04-19 15:05:23 +0200

    Classes toegevoegd, navigationdrawer toegevoegd

---
+++ app/src/main/java/imtpmd/studiebarometer/Advies.java
@@ -0,0 +1,13 @@
+package imtpmd.studiebarometer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by SuperJoot on 19-4-2016.
+ */
+public class Advies extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.advies_main);
+    }
+}
+
--- app/src/main/java/imtpmd/studiebarometer/Vakken.java
+++ app/src/main/java/imtpmd/studiebarometer/Vakken.java
@@ -1,11 +1,11 @@
 package imtpmd.studiebarometer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,15 +13,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+/**
+ * Created by SuperJoot on 19-4-2016.
+ */
+public class Vakken extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.vakken_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -34,39 +37,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_instellingen) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -74,11 +44,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_hoofdmenu) {
-            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            startActivity(new Intent(Vakken.this, MainActivity.class));
         } else if (id == R.id.nav_vakkenencijfers) {
-                startActivity(new Intent(MainActivity.this, Vakken.class));
+            startActivity(new Intent(Vakken.this, Vakken.class));
+
         } else if (id == R.id.nav_advies) {
-                startActivity(new Intent(MainActivity.this, Advies.class));
+            startActivity(new Intent(Vakken.this, Advies.class));
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
